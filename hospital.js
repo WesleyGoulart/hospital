a4a466cccc4032f2debc3f652defdfeba72740b4
Wesley Goulart <goulart89@gmail.com>
2023-03-08 21:17:28 -0300

    Campo convenio funcionando

--- hospital.js
+++ hospital.js
@@ -6,6 +6,7 @@ let campoConvenio
 let nomeConvenio
 let campoCpf
 
+// TODO: Validar nome. Não pode ter números
 campoNome = prompt('Digite o nome: ')
 campoCpf = prompt('Digite o CPF: ')
 if (!cpf.isValid(campoCpf)) {
@@ -13,7 +14,10 @@ if (!cpf.isValid(campoCpf)) {
         campoCpf = prompt('Digite um CPF válido: ')
     }
 }
-campoConvenio = prompt('Tem convênio? ')
-if (campoConvenio == 's') {
+campoConvenio = prompt('Tem convênio? Digite "s" para sim e "n" para não: ')
+while(campoConvenio != 's' && campoConvenio != 'n') {
+    campoConvenio = prompt('Tem convênio? Digite "s" para sim e "n" para não: ')
+}
+if (campoConvenio == 's'){
     nomeConvenio = prompt('Qual o convênio? ')
-} 
+}
