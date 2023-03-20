de5b9f137c457e35f0c01bf8a075ec0b1c8033f3
Wesley Goulart <goulart89@gmail.com>
2023-03-20 11:30:28 -0300

    Salvando dados do cliente

--- cliente.js
+++ cliente.js
@@ -5,19 +5,31 @@ let nomeCliente
 let campoConvenio
 let nomeConvenio
 let campoCpf
+let dados = new Array
+let novoCliente
 
 // TODO: Validar nome. Não pode ter números
-nomeCliente = prompt('Digite o nome: ')
-campoCpf = prompt('Digite o CPF: ')
-if (!cpf.isValid(campoCpf)) {
-    while(!cpf.isValid(campoCpf)) {
-        campoCpf = prompt('Digite um CPF válido: ')
+novoCliente = prompt('Deseja cadastrar um novo cliente? Digite "1" para sim e "2" para encerrar.')
+while (novoCliente == '1') {
+    nomeCliente = prompt('Digite o nome: ')
+    dados = dados + nomeCliente + ','
+    campoCpf = prompt('Digite o CPF: ')
+    if (!cpf.isValid(campoCpf)) {
+        while(!cpf.isValid(campoCpf)) {
+            campoCpf = prompt('Digite um CPF válido: ')
+        }
     }
-}
-campoConvenio = prompt('Tem convênio? Digite "1" para sim e "2" para não: ')
-while(campoConvenio != '1' && campoConvenio != '2') {
+    dados = dados + campoCpf + ','
     campoConvenio = prompt('Tem convênio? Digite "1" para sim e "2" para não: ')
+    while(campoConvenio != '1' && campoConvenio != '2') {
+        campoConvenio = prompt('Tem convênio? Digite "1" para sim e "2" para não: ')
+    }
+    if (campoConvenio == '1'){
+        nomeConvenio = prompt('Qual o convênio? ')
+        dados = dados + nomeConvenio
+    }
+    novoCliente = prompt('Deseja cadastrar um novo cliente? Digite "1" para sim e "2" para encerrar.')
 }
-if (campoConvenio == '1'){
-    nomeConvenio = prompt('Qual o convênio? ')
-}
+
+console.log(dados)
+
