160639c115b415fbb2a114737a2c20d65caa0aa6
Wesley Goulart <goulart89@gmail.com>
2023-03-24 20:43:12 -0300

    Marcar consulta e exame

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const prompt = require('prompt-sync')()
 const moduloCliente = require('./cliente')
 const moduloMedico = require('./medico')
 const moduloExame = require('./exame')
+const moduloMarcarConsultaExame = require('./marcacaoconsultaexame')
 
 let comando
 
-comando = prompt('Para cadastrar cliente, digite "1". Para cadastrar um médico, digite "2". Para cadastrar um exame, digite "3": ')
+comando = prompt('Para cadastrar cliente, digite "1". Para cadastrar um médico, digite "2". Para cadastrar um exame, digite "3". Para marcar um exame ou uma consulta, digite "4".: ')
 
 if (comando == '1') {
     moduloCliente.cadastroCliente()
@@ -17,4 +18,8 @@ if (comando == '2'){
 
 if (comando == '3') {
     moduloExame.cadastroExame()
+}
+
+if(comando == '4') {
+    moduloMarcarConsultaExame.controller()
 }
--- marcacaoconsultaexame.js
+++ marcacaoconsultaexame.js
@@ -1,50 +1,101 @@
 const prompt = require('prompt-sync')()
 const { cpf } = require('cpf-cnpj-validator');
 
-let marcar
 let marcarConsulta
 let marcarExame
 let crmMedico
 let codigoExame
-
-marcar = prompt('Deseja marcar consulta ou exame? Digite "1" para consulta e "2" para exame.: ')
-while(marcar != '1' && marcar != '2') {
-    marcar = prompt('Digite "1" para consulta e "2" para exame.: ')
+let dados = new Array
+let consultaObj = {
+    campoCpf: '',
+    crmMedico: '',
+    codigoExame: ''
+}
+let exameObj = {
+    campoCpf: '',
+    crmMedico: '',
+    codigoExame: ''
 }
 
-if (marcar == '1') {
-    while (marcarConsulta != '1' && marcarConsulta != '2' && marcarConsulta != '3') {
-        marcarConsulta = prompt('Deseja marcar a consulta pelo CPF do paciente (1), CRM do médico (2) ou pelo código do exame (3)?: ')
+function marcarConsultaExame(marcar) {
+    while(marcar != '1' && marcar != '2') {
+        marcar = prompt('Digite "1" para consulta e "2" para exame.: ')
     }
-    if (marcarConsulta == '1') {
-        campoCpf = prompt('Digite o CPF: ')
-        if (!cpf.isValid(campoCpf)) {
-            while(!cpf.isValid(campoCpf)) {
-                campoCpf = prompt('Digite um CPF válido: ')
-            }
+
+    if (marcar == '1') {
+        while (marcarConsulta != '1' && marcarConsulta != '2' && marcarConsulta != '3') {
+            marcarConsulta = prompt('Deseja marcar a consulta pelo CPF do paciente (1), CRM do médico (2) ou pelo código do exame (3)?: ')
         }
-    } else if (marcarConsulta == '2') {
-        crmMedico = parseInt(prompt('Digite o CRM do médico: '))
-    } else if (marcarConsulta == '3') {
-        codigoExame = (prompt('Digite o código do exame: '))
-    }
-}
-
-if (marcar == '2') {
-    while (marcarExame != '1' && marcarExame != '2' && marcarExame != '3') {
-        marcarExame = prompt('Deseja marcar a consulta pelo CPF do paciente (1), CRM do médico (2) ou pelo código do exame (3)?: ')
-    }
-    if (marcarExame == '1') {
-        campoCpf = prompt('Digite o CPF: ')
-        if (!cpf.isValid(campoCpf)) {
-            while(!cpf.isValid(campoCpf)) {
-                campoCpf = prompt('Digite um CPF válido: ')
+        if (marcarConsulta == '1') {
+            campoCpf = prompt('Digite o CPF: ')
+            if (!cpf.isValid(campoCpf)) {
+                while(!cpf.isValid(campoCpf)) {
+                    campoCpf = prompt('Digite um CPF válido: ')
+                }
             }
+            consultaObj.campoCpf = campoCpf
+        } else if (marcarConsulta == '2') {
+            crmMedico = parseInt(prompt('Digite o CRM do médico: '))
+            consultaObj.crmMedico = crmMedico
+        } else if (marcarConsulta == '3') {
+            codigoExame = (prompt('Digite o código do exame: '))
+            consultaObj.codigoExame = codigoExame
         }
-    } else if (marcarExame == '2') {
-        crmMedico = parseInt(prompt('Digite o CRM do médico: '))
-    } else if (marcarExame == '3') {
-        codigoExame = (prompt('Digite o código do exame: '))
+        
+        dados.push(consultaObj)
+
+            consultaObj = {
+            campoCpf: '',
+            crmMedico: '',
+            codigoExame: ''
+            }
+    }
+        
+
+    if (marcar == '2') {
+        while (marcarExame != '1' && marcarExame != '2' && marcarExame != '3') {
+            marcarExame = prompt('Deseja marcar o exame pelo CPF do paciente (1), CRM do médico (2) ou pelo código do exame (3)?: ')
+        }
+        if (marcarExame == '1') {
+            campoCpf = prompt('Digite o CPF: ')
+            if (!cpf.isValid(campoCpf)) {
+                while(!cpf.isValid(campoCpf)) {
+                    campoCpf = prompt('Digite um CPF válido: ')
+                }
+            }
+            exameObj.campoCpf = campoCpf
+        } else if (marcarExame == '2') {
+            crmMedico = parseInt(prompt('Digite o CRM do médico: '))
+            exameObj.crmMedico = crmMedico
+        } else if (marcarExame == '3') {
+            codigoExame = (prompt('Digite o código do exame: '))
+            exameObj.codigoExame = codigoExame
+        }
+
+        dados.push(exameObj)
+
+            exameObj = {
+            campoCpf: '',
+            crmMedico: '',
+            codigoExame: ''
+            }
+    }
+    console.log(dados)
+}
+
+function controller() {
+    let comando
+    while (comando != '3') {
+        comando = prompt ('Deseja marcar consulta ou exame? Digite "1" para consulta, "2" para exame ou "3" para finalizar.: ')
+        if (comando == '1' || comando == '2') {
+            marcarConsultaExame(comando)
+        }
+        if (comando == '3') {
+            return
+        }
     }
 }
 
+module.exports = {
+    controller
+}
