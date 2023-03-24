481a535224c03abac453abd05491f886a03c5472
Wesley Goulart <goulart89@gmail.com>
2023-03-24 16:49:36 -0300

    Salvando exames

--- exame.js
+++ exame.js
@@ -4,11 +4,43 @@ let nomeExame
 let codigoExame
 let valorExameSemConvenio
 let valorExameComConvenio
+let dados = new Array
+let novoExame
+let exameObj = {
+    nomeExame: '',
+    codigoExame: '',
+    valorExameSemConvenio: '',
+    valorExameComConvenio: ''
+}
 
 //Código e valor do exame tem que ser em números
+function cadastroExame() {
+    novoExame = '1'
+    while (novoExame == '1') {
+        nomeExame = prompt('Digite o nome do exame: ')
+        exameObj.nomeExame = nomeExame
+        codigoExame = prompt('Digite o código do exame: ')
+        exameObj.codigoExame = codigoExame
+        valorExameSemConvenio = prompt('Valor do exame sem convênio: ')
+        exameObj.valorExameSemConvenio = valorExameSemConvenio
+        valorExameComConvenio = valorExameSemConvenio - (valorExameSemConvenio * 0.3)
+        console.log('Valor com convênio: ', valorExameComConvenio)
+        exameObj.valorExameComConvenio = valorExameComConvenio
 
-nomeExame = prompt('Digite o nome do exame: ')
-codigoExame = prompt('Digite o código do exame: ')
-valorExameSemConvenio = prompt('Valor do exame sem convênio: ')
-valorExameComConvenio = valorExameSemConvenio - (valorExameSemConvenio * 0.3)
-console.log('Valor com convênio: ', valorExameComConvenio)
+        dados.push(exameObj)
+
+        exameObj = {
+            nomeExame: '',
+            codigoExame: '',
+            valorExameSemConvenio: '',
+            valorExameComConvenio: ''
+        }
+
+        novoExame = prompt('Deseja cadastrar um novo exame? Digite "1" para sim e "2" para encerrar.')
+    }
+    console.log(dados)
+}
+
+module.exports = {
+    cadastroExame
+}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const prompt = require('prompt-sync')()
 const moduloCliente = require('./cliente')
 const moduloMedico = require('./medico')
+const moduloExame = require('./exame')
 
 let comando
 
-comando = prompt('Para cadastrar cliente, digite "1". Para cadastrar um médico, digite "2": ')
+comando = prompt('Para cadastrar cliente, digite "1". Para cadastrar um médico, digite "2". Para cadastrar um exame, digite "3": ')
 
 if (comando == '1') {
     moduloCliente.cadastroCliente()
@@ -12,4 +13,8 @@ if (comando == '1') {
 
 if (comando == '2'){
     moduloMedico.cadastroMedico()
+}
+
+if (comando == '3') {
+    moduloExame.cadastroExame()
 }
